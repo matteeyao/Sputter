dd3068d66f7180f18616e2c3798a4a10792ef022
matteeyao <myaoski@gmail.com>
2021-05-14 11:47:36 -0500

    Add register and login functions

--- backend/app.js
+++ backend/app.js
@@ -12,26 +12,36 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-/* This creates a new Express server. Now let's setup a basic route so that we
-can render some information on our page. */
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Hello World")); /* (1) */
+
+app.use(bodyParser.urlencoded({ extended: false })); /* (2) */
+app.use(bodyParser.json());
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
-/* Setup some middleware for body parser */
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const port = process.env.PORT || 5000; /* (3) */
 
-/* Before we can run the server, we need to tell our app which port to run on. 
+app.listen(port, () => console.log(`Server is running on port ${port}`)); /* (4) */
+
+/*----------------------------------------------------------------------------*/
+
+/*
+(1) This creates a new Express server. Now let's setup a basic route so that we
+can render some information on our page.
+
+(2) Setup some middleware for body parser.
+
+(3) Before we can run the server, we need to tell our app which port to run on. 
 Keeping in mind that we will later be deploying our app to Heroku, which 
 requires us to run our server on process.env.PORT, add the following line to 
-app.js: */
-const port = process.env.PORT || 5000;
-/* Locally our server will now run on localhost:5000. */
+app.js.
 
-/* Finally, let's tell Express to start a socket and listen for connections on 
-the path. Do so by adding the following line to your file: */
-app.listen(port, () => console.log(`Server is running on port ${port}`));
-/* This will also log a success message to the console when our server is 
-running successfully. */
+Locally our server will now run on localhost:5000.
+
+(4) Finally, let's tell Express to start a socket and listen for connections on 
+the path. Do so by adding the following line to your file.
+
+This will also log a success message to the console when our server is 
+running successfully. 
+*/
